efb25227bc699c8a921c997140922235f12f01e7
ibandominguez <domingueznoda@gmail.com>
2022-11-28 09:29:17 +0000

    Removed deprecated option

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -65,7 +65,6 @@ lvim.keys.insert_mode["jk"] = "<Esc>"
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
