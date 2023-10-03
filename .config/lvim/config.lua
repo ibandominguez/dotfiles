7974ad6c29785164287ca6e0541fb41ed88372bf
Ibán Domínguez Noda <ibandominguez@MacBook-Air-de-Iban.local>
2023-10-03 01:33:55 +0100

    Cleaned up and formated lvim config file

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,152 +1,52 @@
---[[
-
-lvim is the global options object
-
-Linters should be filled in as strings with either
-a global executable or a path to an executable
-
---]]
-
--- general
+-- General config
 lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "onedarker"
--- lvim.use_icons = false -- to disable icons and use a minimalist setup, uncomment the following
-
--- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
 
--- Refer to https://www.lunarvim.org/docs/keybind-overview for defaults table
--- Quick reminder commands:
--- <C-\>: Toggle terminal
--- Normal: <Tab>,<S-Tab>: Navigate tabs
--- Normal: <C-h,j,k,l>: Navigate windows
-
--- add your own keymapping
+-- Keymapping setup
 lvim.keys.normal_mode["<Tab>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-Tab>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<C-\\>"] = ":ToggleTerm<CR>"
 
--- unmap a default keymapping
+-- Keymapping unset
 -- vim.keymap.del("n", "<C-Up>")
 
--- override a default keymapping
+-- Keymapping override
 -- lvim.keys.normal_mode["<C-q>"] = ":q<cr>" -- or vim.keymap.set("n", "<C-q>", ":q<cr>" )
 
--- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
--- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
-
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
-
--- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
--- lvim.builtin.which_key.mappings["t"] = {
---   name = "+Trouble",
---   r = { "<cmd>Trouble lsp_references<cr>", "References" },
---   f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
---   d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
---   q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
---   l = { "<cmd>Trouble loclist<cr>", "LocationList" },
---   w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
--- }
-
--- TODO: User Config for predefined plugins
--- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+-- Builtin plugins configs (Reset LunarVim, Run :Lazy install changes)
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
-
--- if you don't want all the parsers change this to a table of the ones you want
-lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "markdown",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
-  "php",
-}
-
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
--- generic LSP settings
-
--- make sure server will always be installed even if the server is in skipped_servers list
+-- Lsp Setting, ensured installed
 lvim.lsp.installer.setup.ensure_installed = {
+  "bashls",
+  "cssls",
+  "html",
   "tailwindcss",
   "eslint",
   "tsserver",
   "jsonls",
+  "pylsp"
 }
-
--- -- change UI setting of `LspInstallInfo`
--- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
--- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
--- lvim.lsp.installer.setup.ui.border = "rounded"
--- lvim.lsp.installer.setup.ui.keymaps = {
---     uninstall_server = "d",
---     toggle_server_expand = "o",
--- }
-
--- ---@usage disable automatic installation of servers
--- lvim.lsp.installer.setup.automatic_installation = false
-
--- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
--- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
--- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
--- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pyright", opts)
-
--- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
--- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
--- vim.tbl_map(function(server)
---   return server ~= "emmet_ls"
--- end, lvim.lsp.automatic_configuration.skipped_servers)
-
--- -- you can set a custom on_attach function that will be used for all the language servers
--- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
--- lvim.lsp.on_attach_callback = function(client, bufnr)
---   local function buf_set_option(...)
---     vim.api.nvim_buf_set_option(bufnr, ...)
---   end
---   --Enable completion triggered by <c-x><c-o>
---   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
--- end
 
 -- Formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
 
 formatters.setup {
   {
-    command = "prettier",
+    name = "prettier",
     args = { "--print-width", "100", "--trailing-comma", "none", "--no-semi", "--single-quote" },
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
   },
   {
-    command = "phpcsfixer",
+    name = "phpcsfixer",
     filetypes = { "php" },
   },
 }
@@ -156,11 +56,11 @@ local linters = require "lvim.lsp.null-ls.linters"
 
 linters.setup {
   {
-    command = "standardjs",
+    name = "standardjs",
     filetypes = { "javascript", "javascriptreact" },
   },
   {
-    command = "tsc",
+    name = "tsc",
     filetypes = { "typescript", "typescriptreact" },
   },
 }
@@ -172,18 +72,21 @@ lvim.plugins = {
   { "christoomey/vim-tmux-navigator" }, -- Tmux and Vim navigation using Ctrl+(jkhl)
   { "tpope/vim-surround" },             -- https://github.com/tpope/vim-surround
   { 'MunifTanjim/nui.nvim' },           -- Cmd dialog input dependencies
-  {                                     -- Live Diagnostics on typing
+  -- Live Diagnostics on typing
+  {
     "ray-x/lsp_signature.nvim",
     event = "BufRead",
     config = function() require "lsp_signature".on_attach() end,
   },
-  { -- Auto close and renames tags
+  -- Auto close and renames tags
+  {
     "windwp/nvim-ts-autotag",
     config = function()
       require("nvim-ts-autotag").setup()
     end
   },
-  { -- Experimental UI (Command line, search and messages)
+  -- Experimental UI (Command line, search and messages)
+  {
     "folke/noice.nvim",
     event = "VeryLazy",
     config = function()
