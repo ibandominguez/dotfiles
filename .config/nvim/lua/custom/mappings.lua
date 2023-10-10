f911676d65f52d0ceb76ae685ccad7c17d99a634
Ibán Domínguez Noda <domingueznoda@gmail.com>
2023-10-10 22:38:27 +0100

    Added lunarvim like line diagnostics

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -87,6 +87,17 @@ M.lspconfig = {
       end,
       "LSP SignatureHelp",
     },
+    ["gl"] = {
+      function()
+        local float = vim.diagnostic.config().float
+        if float then
+          local config = type(float) == "table" and float or {}
+          config.scope = "line"
+          vim.diagnostic.open_float(config)
+        end
+      end,
+      "Show line diagnostics",
+    },
   },
 }
 
