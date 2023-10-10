e9f61d72b6f7e3274d8608a537685e6f91210727
Ibán Domínguez Noda <domingueznoda@gmail.com>
2023-10-11 00:36:28 +0100

    Changed nvim theme

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "aquarium",
-  theme_toggle = { "aquarium", "one_light" },
+  theme = "github_dark",
+  theme_toggle = { "github_dark", "github_light" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
