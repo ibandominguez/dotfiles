ee2ecd0aa86d35fe29e3d45db54c5a83e1ab967d
Ibán Domínguez <domingueznoda@gmail.com>
2025-06-07 10:42:54 +0100

    Fixed the issue on loosing the focus on signature help popups

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -121,6 +121,9 @@ return {
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
           },
+          signature = {
+            enabled = false,
+          },
         },
         presets = {
           bottom_search = true, -- use a classic bottom cmdline for search
